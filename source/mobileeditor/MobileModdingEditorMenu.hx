fde55dbabf37db1348921a8ccbae3eb38996e7aa
Vitkkk <asstecbp@gmail.com>
2026-08-15 15:38:12 -0300

    Require explicit mod selection before mobile Week editing

--- source/mobileeditor/MobileModdingEditorMenu.hx
+++ source/mobileeditor/MobileModdingEditorMenu.hx
@@ -10,6 +10,8 @@ import mobileeditor.week.MobileWeekListState;
 /** Entry point kept separate from Psych Engine's original editor menu. */
 class MobileModdingEditorMenu extends MusicBeatState
 {
+    var projectText:FlxText;
+
     override function create():Void
     {
         super.create();
@@ -25,11 +27,27 @@ class MobileModdingEditorMenu extends MusicBeatState
         subtitle.setFormat(Paths.font('vcr.ttf'), 19, 0xFFB8C3D9, LEFT);
         add(subtitle);
 
-        add(new MobileButton(50, 150, FlxG.width - 100, 82, 'MOBILE WEEK EDITOR', function() {
-            MusicBeatState.switchState(new MobileWeekListState());
+        projectText = new FlxText(50, 126, FlxG.width - 390, '', 18);
+        projectText.setFormat(Paths.font('vcr.ttf'), 18, 0xFFF9CF51, LEFT);
+        add(projectText);
+        refreshProjectText();
+
+        add(new MobileButton(FlxG.width - 310, 112, 260, 54, 'ESCOLHER MOD', function() {
+            openSubState(new MobileProjectPickerSubState(function(_:String) refreshProjectText()));
         }));
 
-        var future = new FlxText(50, 270, FlxG.width - 100,
+        add(new MobileButton(50, 190, FlxG.width - 100, 82, 'MOBILE WEEK EDITOR', function() {
+            if (MobileProjectContext.activeMod().length == 0)
+            {
+                openSubState(new MobileProjectPickerSubState(function(_:String) {
+                    refreshProjectText();
+                    MusicBeatState.switchState(new MobileWeekListState());
+                }));
+            }
+            else MusicBeatState.switchState(new MobileWeekListState());
+        }));
+
+        var future = new FlxText(50, 310, FlxG.width - 100,
             'Chart Editor Mobile - futuramente\nCharacter Editor Mobile - futuramente\nStage Editor Mobile - futuramente', 24);
         future.setFormat(Paths.font('vcr.ttf'), 24, 0xFF6F788A, LEFT);
         add(future);
@@ -37,6 +55,12 @@ class MobileModdingEditorMenu extends MusicBeatState
         add(new MobileButton(36, FlxG.height - 84, 190, 56, '< VOLTAR', function() {
             MusicBeatState.switchState(new editors.MasterEditorMenu());
         }));
+    }
+
+    function refreshProjectText():Void
+    {
+        var project = MobileProjectContext.activeMod();
+        projectText.text = project.length > 0 ? 'MOD ATUAL: ' + project : 'MOD ATUAL: nenhum selecionado';
     }
 
     override function update(elapsed:Float):Void
