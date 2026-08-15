b7689a2dc4ce1e8f4f609ea685cc93ae5959e119
Vitkkk <asstecbp@gmail.com>
2026-08-15 11:44:23 -0300

    Integrate visual Chart character picker

--- tools/patch_chart_mobile_events.py
+++ tools/patch_chart_mobile_events.py
@@ -5,18 +5,27 @@ p = Path('source/editors/ChartingState.hx')
 s = p.read_text()
 
 old = "import mobileeditor.MobileSafeWriter;\nimport mobileeditor.MobileEditorSavePaths;"
-new = "import mobileeditor.MobileSafeWriter;\nimport mobileeditor.MobileEditorSavePaths;\n#if android\nimport mobileeditor.events.MobileEventEditorSubState;\nimport mobileeditor.ui.MobileButton;\n#end"
+new = "import mobileeditor.MobileSafeWriter;\nimport mobileeditor.MobileEditorSavePaths;\n#if android\nimport mobileeditor.events.MobileEventEditorSubState;\nimport mobileeditor.chart.MobileChartCharactersSubState;\nimport mobileeditor.ui.MobileButton;\n#end"
 if 'MobileEventEditorSubState' not in s:
     if old not in s:
         raise SystemExit('import anchor missing')
     s = s.replace(old, new, 1)
+elif 'import mobileeditor.chart.MobileChartCharactersSubState;' not in s:
+    s = s.replace('import mobileeditor.events.MobileEventEditorSubState;',
+                  'import mobileeditor.events.MobileEventEditorSubState;\nimport mobileeditor.chart.MobileChartCharactersSubState;', 1)
 
 if "'+ EVENTO', openMobileEventEditor" not in s:
     match = re.search(r'(?m)^[ \t]*this\.add\(_pad\);[ \t]*$', s)
     if match is None:
         raise SystemExit('create anchor missing')
-    insert = "\n\n\t\t#if android\n\t\tvar mobileEventButton = new MobileButton(FlxG.width - 310, FlxG.height - 72, 280, 52, '+ EVENTO', openMobileEventEditor);\n\t\tmobileEventButton.scrollFactor.set();\n\t\tadd(mobileEventButton);\n\t\t#end"
+    insert = "\n\n\t\t#if android\n\t\tvar mobileCharactersButton = new MobileButton(FlxG.width - 610, FlxG.height - 72, 280, 52, 'PERSONAGENS', openMobileChartCharacters);\n\t\tmobileCharactersButton.scrollFactor.set();\n\t\tadd(mobileCharactersButton);\n\t\tvar mobileEventButton = new MobileButton(FlxG.width - 310, FlxG.height - 72, 280, 52, '+ EVENTO', openMobileEventEditor);\n\t\tmobileEventButton.scrollFactor.set();\n\t\tadd(mobileEventButton);\n\t\t#end"
     s = s[:match.end()] + insert + s[match.end():]
+elif "'PERSONAGENS', openMobileChartCharacters" not in s:
+    event_button = "\t\tvar mobileEventButton = new MobileButton(FlxG.width - 310, FlxG.height - 72, 280, 52, '+ EVENTO', openMobileEventEditor);"
+    chars_button = "\t\tvar mobileCharactersButton = new MobileButton(FlxG.width - 610, FlxG.height - 72, 280, 52, 'PERSONAGENS', openMobileChartCharacters);\n\t\tmobileCharactersButton.scrollFactor.set();\n\t\tadd(mobileCharactersButton);\n" + event_button
+    if event_button not in s:
+        raise SystemExit('event button anchor missing')
+    s = s.replace(event_button, chars_button, 1)
 
 method_anchor = "\n\tfunction updateZoom() {"
 method = '''
@@ -48,17 +57,36 @@ method = '''
 \t\t\t\tupdateGrid();
 \t\t\t}));
 \t}
+
+\tfunction openMobileChartCharacters():Void
+\t{
+\t\topenSubState(new MobileChartCharactersSubState(_song.player1, _song.player2, _song.player3,
+\t\t\tfunction(player:String, opponent:String, girlfriend:String)
+\t\t\t{
+\t\t\t\t_song.player1 = player;
+\t\t\t\t_song.player2 = opponent;
+\t\t\t\t_song.player3 = girlfriend;
+\t\t\t\tupdateHeads();
+\t\t\t}));
+\t}
 \t#end
 '''
 if 'function openMobileEventEditor():Void' not in s:
     if method_anchor not in s:
         raise SystemExit('method anchor missing')
     s = s.replace(method_anchor, method + method_anchor, 1)
+else:
+    # Repair first callback closure if present.
+    broken = "\t\t\t\tupdateGrid();\n\t\t\t});\n\t}\n\t#end\n\n\tfunction updateZoom() {"
+    fixed = "\t\t\t\tupdateGrid();\n\t\t\t}));\n\t}\n\t#end\n\n\tfunction updateZoom() {"
+    if broken in s:
+        s = s.replace(broken, fixed, 1)
 
-# Repair the first integration revision, which closed only the constructor call.
-broken = "\t\t\t\tupdateGrid();\n\t\t\t});\n\t}\n\t#end\n\n\tfunction updateZoom() {"
-fixed = "\t\t\t\tupdateGrid();\n\t\t\t}));\n\t}\n\t#end\n\n\tfunction updateZoom() {"
-if broken in s:
-    s = s.replace(broken, fixed, 1)
+    if 'function openMobileChartCharacters():Void' not in s:
+        old_end = "\t\t\t}));\n\t}\n\t#end\n\n\tfunction updateZoom() {"
+        new_end = "\t\t\t}));\n\t}\n\n\tfunction openMobileChartCharacters():Void\n\t{\n\t\topenSubState(new MobileChartCharactersSubState(_song.player1, _song.player2, _song.player3,\n\t\t\tfunction(player:String, opponent:String, girlfriend:String)\n\t\t\t{\n\t\t\t\t_song.player1 = player;\n\t\t\t\t_song.player2 = opponent;\n\t\t\t\t_song.player3 = girlfriend;\n\t\t\t\tupdateHeads();\n\t\t\t}));\n\t}\n\t#end\n\n\tfunction updateZoom() {"
+        if old_end not in s:
+            raise SystemExit('chart character method anchor missing')
+        s = s.replace(old_end, new_end, 1)
 
 p.write_text(s)
